a0ef152aed6da06b987c6040226e10062fc5b0cb
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-10-27 20:39:45 +0200

    add `alpha` tag to `stats`

--- commands/main/stats.js
+++ commands/main/stats.js
@@ -7,7 +7,7 @@ $addField[Node.js;$nodeVersion;yes]
 $addField[Cpu;$cpu;yes]
 $addField[Ram;$ram;yes]
 $addField[Ping;$pingms;yes]
-$addField[Version;v3.0.2;yes]
+$addField[Version;v3.0.2-alpha;yes]
 $addField[Uptime;$uptime[humanize];yes]
 $addField[Library;Aoi.js;yes]
 `
