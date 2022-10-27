1b3c3d0f5ed1d896c16990493875d9fbfe83ef7a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-10-27 20:36:55 +0200

    3.0.2 👀

--- commands/main/help.js
+++ commands/main/help.js
@@ -1,6 +1,7 @@
 module.exports = {
 name: "help",
 code: `$title[Help Commands]
+$addField[settings;manage kirbo's chatbot Reply settings]
 $addField[stats;returns stats information about the bot;no]
 $addField[resetchatbot;resets the chatbot channel!;no]
 $addField[setchatbot;set the chatbot channel!;no]
--- commands/main/stats.js
+++ commands/main/stats.js
@@ -7,7 +7,7 @@ $addField[Node.js;$nodeVersion;yes]
 $addField[Cpu;$cpu;yes]
 $addField[Ram;$ram;yes]
 $addField[Ping;$pingms;yes]
-$addField[Version;v3.0.1;yes]
+$addField[Version;v3.0.2;yes]
 $addField[Uptime;$uptime[humanize];yes]
 $addField[Library;Aoi.js;yes]
 `
