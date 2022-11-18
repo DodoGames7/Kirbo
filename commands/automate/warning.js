12991390a98cf22861453c3f6e19ddeea30c8fff
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-11-18 23:53:55 +0200

    Update warning.js

--- commands/automate/warning.js
+++ commands/automate/warning.js
@@ -1,4 +1,5 @@
 module.exports = {
+channel: "",
 type: "ready",
 code: `Hey!, this is currently unstable this release is for testing purposes only if you want to use the stable version then you may be looking for either 3.0.1 or 3.0.2-preview for now!
 `
