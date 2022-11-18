7e2d13b4ef1e31bfe51d656ec4df0776d7e27474
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-11-18 23:57:35 +0200

    updating

--- commands/main/support.js
+++ commands/main/support.js
@@ -3,5 +3,5 @@ module.exports = {
     code: `$title[Support Server]
     $description[There you Go!, click on the button below to join]
     $color[d74894]
-    $addButton[1;Support;link;https://discord.gg/pFwKjAaZvj;no]`
+    $addButton[1;Support;Link;https://discord.gg/pFwKjAaZvj;no]`
     }
