83c5ab12bf9619971a75b26ade94913ebcdd7c3c
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2022-12-15 00:29:27 +0200

    i forgor

--- commands/main/stats.js
+++ commands/main/stats.js
@@ -7,7 +7,7 @@ module.exports = {
     $addField[Cpu;$cpu;yes]
     $addField[Ram;$ram;yes]
     $addField[Ping;$pingms;yes]
-    $addField[Version;v3.0.2-preview;yes]
+    $addField[Version;v3.0.2;yes]
     $addField[Uptime;$uptime[humanize];yes]
     $addField[Library;Aoi.js;yes]
     `
