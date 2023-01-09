b1f6dc9b5e0eecde79c1984d11f0ca28616588c4
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-01-09 18:14:47 +0200

    revert stats to it's current state

--- commands/main/stats.js
+++ commands/main/stats.js
@@ -1,15 +1,14 @@
 module.exports = {
     name: "stats",
     code: `$title[bot info about $username[$clientID]]
-    $description[
-    User Count: **$allMembersCount**
-    Server Count: **$serverCount
-    Node.js: **$nodeVersion**
-    Cpu: \`$cpu\`
-    Ping: \`$ping\`ms
-    Version: 3.0.3-alpha
-    Uptime: $uptime[humanize]
-    Library: Aoi.js
-    ]
+    $addField[User Count;$allMembersCount;yes]
+    $addField[Server Count;$serverCount;yes]
+    $addField[Node.js;$nodeVersion;yes]
+    $addField[Cpu;$cpu;yes]
+    $addField[Ram;$round[$ram];yes]
+    $addField[Ping;$pingms;yes]
+    $addField[Version;v3.0.4-alpha;yes]
+    $addField[Uptime;$uptime[humanize];yes]
+    $addField[Library;Aoi.js;yes]
     `
     }
