4592f156be625203a0a98e1aa68be47323a5a07d
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-02-18 22:07:24 +0200

    make channel selection author only

--- commands/automate/setchatreply.js
+++ commands/automate/setchatreply.js
@@ -1,5 +1,4 @@
 module.exports = {
-    name: "channelselect",
     type: "interaction",
     prototype: "selectMenu",
     code: `$setGuildVar[chatbot;$interactionData[values[0]];$guildID]
@@ -7,5 +6,18 @@ module.exports = {
     $interactionReply[you can now talk with me in <#$interactionData[values[0]]>;;;;all;true]
     $onlyIf[$getGuildVar[isset]!=$interactionData[values[0]];you already let me talk in that channel
     {options:{ephemeral: true}}
-    {extraOptions:{interaction: true}}]`
+    {extraOptions:{interaction: true}}]
+    
+    $onlyif[$get[authorID]==$interactionData[author.id];
+  You aren't the author of this interaction.
+{options:{ephemeral: true}}
+{extraOptions:{interaction: true}}
+]
+
+$onlyif[$get[customId]==channelselect;]
+
+$let[authorID;$splitText[2]]
+$let[customId;$splitText[1]] 
+$textSplit[$interactionData[customId];_]
+`
 }
--- commands/main/setchatbot.js
+++ commands/main/setchatbot.js
@@ -2,7 +2,7 @@ module.exports = {
     name: "setchatbot",
     code: `
            $sendMessage[{newEmbed:{title:Choose a Channel}{description:click on the Select menu to choose an channel for me to talk!}{color:d74894}}
-            {actionRow:{channelInput:channelselect:make a selection:1:1:no:{channelType:0}}}]
+            {actionRow:{channelInput:channelselect_$authorID:make a selection:1:1:no:{channelType:0}}}]
       $onlyPerms[manageguild;you need \`ManageServer\` permission in order to use this]
       
       
