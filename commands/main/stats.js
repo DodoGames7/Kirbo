5b5664b7dff3fb003b0ec0e396a9feb6dee2713a
dodoGames <69091682+DodoGames7@users.noreply.github.com>
2023-04-17 17:03:49 +0000

    fix stats cmd

--- commands/main/stats.js
+++ commands/main/stats.js
@@ -3,10 +3,10 @@ module.exports = {
     aliases: "botinfo",
     code: `$title[bot info about $username[$clientID]]
     $addField[User Count;$allMembersCount;true]
-    $addField[Server Count;$serverCount;true]
+    $addField[Server Count;$guildCount;true]
     $addField[Node.js;$nodeVersion;true]
     $addField[Cpu;$cpu;true]
-    $addField[Ram;$round[$ram];true]
+    $addField[Ram;$round[$ram]%;true]
     $addField[Ping;$pingms;true]
     $addField[Version;v3.0.4-preview;true]
     $addField[Uptime;$uptime[humanize];true]
